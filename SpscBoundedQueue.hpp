bf4176c07c94850b1911474dc0eb6f6fc42f7130
Nick Strupat <nick.strupat@gmail.com>
2014-10-20 14:39:30 -0400

    commit class template header file

---
+++ SpscBoundedQueue.hpp
@@ -0,0 +1,60 @@
+#ifndef CONCURRENT_LOCK_FREE_SPSC_BOUNDED_QUEUE_HPP_INCLUDED
+#define CONCURRENT_LOCK_FREE_SPSC_BOUNDED_QUEUE_HPP_INCLUDED
+
+#include <atomic>
+#include <cstddef>
+#include <memory>
+
+namespace Concurrent {
+	namespace LockFree {
+		template<typename T>
+		class SpscBoundedQueue {
+			std::unique_ptr<T[]> buffer;
+			std::size_t capacity;
+			std::atomic<std::size_t> size;
+			std::size_t readIndex;
+			std::size_t writeIndex;
+		public:
+			// Constructors
+			SpscBoundedQueue(SpscBoundedQueue const &) = delete;
+			SpscBoundedQueue const & operator=(SpscBoundedQueue const &) = delete;
+			explicit SpscBoundedQueue(std::size_t capacity)
+				: capacity(capacity)
+				, buffer(new T[capacity])
+				, size(0)
+				, readIndex(0)
+				, writeIndex(0)
+			{}
+
+			// Capacity
+			std::size_t Capacity() const { return capacity; }
+			std::size_t Size() const { return size; }
+			bool Empty() const { return Size() == 0; }
+			bool Full() const { return Size() == Capacity(); }
+
+			// Modifiers
+			bool Push(T const & source) {
+				if (Full())
+					return false;
+				buffer[writeIndex] = source;
+				++size;
+				++writeIndex;
+				if (writeIndex == Capacity())
+					writeIndex = 0;
+				return true;
+			}
+			bool Pop(T & destination) {
+				if (Empty())
+					return false;
+				destination = buffer[readIndex];
+				--size;
+				++readIndex;
+				if (readIndex == Capacity())
+					readIndex = 0;
+				return true;
+			}
+		};
+	}
+}
+
+#endif
